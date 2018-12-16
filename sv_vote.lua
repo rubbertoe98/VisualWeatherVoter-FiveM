93f18b4f9c39a6bcc5576fff6842300ac641f0f7
rubbertoe98 <robertobarra98@gmail.com>
2018-12-16 23:11:18 +0000

    initial commit

---
+++ sv_vote.lua
@@ -0,0 +1,35 @@
+voteCooldown = 1800
+currentWeather = "EXTRASUNNY"
+
+weatherVoterCooldown = voteCooldown
+
+RegisterServerEvent("CMG:vote") 
+AddEventHandler("CMG:vote", function(weatherType)
+    TriggerClientEvent("CMG:voteStateChange",-1,weatherType)
+end)
+
+RegisterServerEvent("CMG:tryStartWeatherVote") 
+AddEventHandler("CMG:tryStartWeatherVote", function()
+	local source = source
+    if weatherVoterCooldown >= voteCooldown then
+        TriggerClientEvent("CMG:startWeatherVote", -1)
+        weatherVoterCooldown = 0
+    else
+		TriggerClientEvent("chatMessage",source,"Another vote can be started in " .. tostring(voteCooldown-weatherVoterCooldown) .. " seconds!",{255, 0, 0})
+    end
+end)
+
+RegisterServerEvent("CMG:getCurrentWeather") 
+AddEventHandler("CMG:getCurrentWeather", function()
+    local source = source
+    TriggerClientEvent("CMG:voteFinished",source,currentWeather)
+end)
+
+
+Citizen.CreateThread(function()
+	while true do
+		weatherVoterCooldown = weatherVoterCooldown + 1
+		Citizen.Wait(1000)
+	end
+end)
+
